11902b460cbdd024c830bc974ecc3abc6554d453
Luizsoh <souza_luiz@outlook.com.br>
2017-09-23 09:31:36 -0300

    Minor changes

--- AppWeb/Web/Controllers/HomeController.cs
+++ AppWeb/Web/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult ListPresen()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
